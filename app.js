510d5cfe2db98b183b0c8874f7230e986fad5da8
cale312 <zikisacalesogaxa@gmail.com>
2017-04-07 15:23:35 +0200

    first commit

--- app.js
+++ app.js
@@ -1,157 +1,147 @@
-//document.querySelector('.stock-info').innerHTML = localStorage.getItem('stock-info');
 
-var brandSelector = document.querySelector('.brand');
-var sizeSelector = document.querySelector('.size');
-var colorSelector = document.querySelector('.color');
+var admin = [{usrnm: 'cale', pswd: '123'}];
+var userName = document.querySelector('.usrname-text');
+var passWord = document.querySelector('.pswd-text');
+var stock = [
+    {brand: "Adidas", size: "US/7", color: "White", stock: "20", price: "R2 500"},
+    {brand: "Mizuno", size: "US/9", color: "Red", stock: "15", price: "R3 000"},
+    {brand: "Nike", size: "US/4", color: "Black", stock: "5", price: "R5 000"}
+];
 
-var brandList = document.getElementsByClassName('brandList');
+var dropDownTemplate = document.querySelector('.drop-down-template').innerHTML;
+var template = Handlebars.compile(dropDownTemplate);
+var data = {list: stock};
+document.querySelector('.stock-checker').innerHTML = template(data);
 
-var sizeList = document.getElementsByClassName('sizeList');
-
-var colorList = document.getElementsByClassName('colorList');
-
-var addBrand = document.querySelector('.add-brand');
-var addSize = document.querySelector('.add-size');
-var addColor = document.querySelector('.add-color');
-var addStock = document.querySelector('.add-stock');
-var addPrice = document.querySelector('.add-price');
-
-var cleatsList = [
-        {brand: 'Adidas', stock: '28', price: 'R2 000', color: 'Red', size: 'US/7'},
-        {brand: 'Adidas', stock: '50', price: 'R2 500', color: 'White', size: 'US/10'},
-        {brand: 'Mizuno', stock: '50', price: 'R3 500', color: 'Blue', size: 'US/9'},
-        {brand: 'Mizuno', stock: '32', price: 'R3 000', color: 'Black', size: 'US/7'},
-        {brand: 'New Balance', stock: '90', price: 'R1 000', color: 'Blue', size: 'US/6'},
-        {brand: 'New Balance', stock: '76', price: 'R1 500', color: 'White', size: 'US/8'},
-        {brand: 'Nike', stock: '35', price: 'R3 900', color: 'Black', size: 'US/10'},
-        {brand: 'Nike', stock: '76', price: 'R3 900', color: 'Red', size: 'US/10'},
-        {brand: 'Under Armor', stock: '58', price: 'R5 700', color: 'White', size: 'US/6'},
-        {brand: 'Under Armor', stock: '67', price: 'R5 000', color: 'Blue', size: 'US/7'}
-    ];
-
-var addedStock = [];
-
-var slideIndex = 0;
-slide();
-
-function slide() {
+document.querySelector('.show-stock-btn').addEventListener('click', function () {
     'use strict';
-    var x = document.getElementsByClassName('cleats');
-    for (var i = 0; i < x.length; i++) {
-        x[i].style.display = 'none';
+    var showTemplate = document.querySelector('.show-stock-template').innerHTML;
+    var temp = Handlebars.compile(showTemplate);
+    document.querySelector('.display-all-stock').innerHTML = temp(data);
+    document.querySelector('.display-all-stock').style.display = 'block';
+});
+document.querySelector('.hide-table-btn').addEventListener('click', function () {
+    'use strict';
+    document.querySelector('.display-all-stock').style.display = 'none';
+});
+document.querySelector('.search-btn').addEventListener('click', function () {
+    'use strict';
+    var searchTemplate = document.querySelector('.search-template').innerHTML;
+    var temp = Handlebars.compile(searchTemplate);
+    var brandOptions = document.querySelector('.brand');
+    var sizeOptions = document.querySelector('.size');
+    var colorOptions = document.querySelector('.color');
+    var stockFound = false;
+    for (var i = 0; i < stock.length; i++) {
+        if (brandOptions.value === stock[i].brand && sizeOptions.value === stock[i].size && colorOptions.value === stock[i].color) {
+            document.querySelector('.searched-stock-display').innerHTML = temp(stock[i]);
+            stockFound = true;
+        }
+        if (stockFound === false) {
+            document.querySelector('.searched-stock-display').innerHTML = '<h3>We currently do not have that in stock!!</h3>';
+        }
     }
-    slideIndex++
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = 'inline-block';
-    setTimeout(slide, 2000);
-}
-
-document.querySelector('.check-btn').addEventListener('click', function () {
+});
+document.querySelector('.update-btn').addEventListener('click', function () {
     'use strict';
-    var myTemplate = document.querySelector('.template').innerHTML;
-    
-    var template = Handlebars.compile(myTemplate);
-    
-    if (brandSelector.value === brandList[0].textContent && sizeSelector.value === sizeList[1].textContent && colorSelector.value === colorList[2].textContent) {
-        
-        document.querySelector('.stock-info').innerHTML = template(cleatsList[0]);
-        
-    } else if (brandSelector.value === brandList[0].textContent && sizeSelector.value === sizeList[4].textContent && colorSelector.value === colorList[3].textContent) {
-        
-        document.querySelector('.stock-info').innerHTML = template(cleatsList[1]);
-        
-    } else if (brandSelector.value === brandList[1].textContent&& sizeSelector.value === sizeList[3].textContent && colorSelector.value === colorList[0].textContent) {
-        
-        document.querySelector('.stock-info').innerHTML = template(cleatsList[2]);
-        
-    } else if (brandSelector.value === brandList[1].textContent && sizeSelector.value === sizeList[1].textContent && colorSelector.value === colorList[1].textContent) {
-        
-        document.querySelector('.stock-info').innerHTML = template(cleatsList[3]);
-        
-    } else if (brandSelector.value === brandList[2].textContent && sizeSelector.value === sizeList[0].textContent && colorSelector.value === colorList[0].textContent) {
-        
-        document.querySelector('.stock-info').innerHTML = template(cleatsList[4]);
-        
-    } else if (brandSelector.value === brandList[2].textContent && sizeSelector.value == sizeList[2].textContent && colorSelector.value === colorList[3].textContent) {
-        
-        document.querySelector('.stock-info').innerHTML = template(cleatsList[5]);
-        
-    } else if (brandSelector.value === brandList[3].textContent && sizeSelector.value === sizeList[4].textContent && colorSelector.value === colorList[1].textContent) {
-        
-        document.querySelector('.stock-info').innerHTML = template(cleatsList[6]);
-        
-    } else if (brandSelector.value === brandList[3].textContent && sizeSelector.value === sizeList[4].textContent && colorSelector.value === colorList[2].textContent) {
-        
-        document.querySelector('.stock-info').innerHTML = template(cleatsList[7]);
-        
-    } else if (brandSelector.value === brandList[4].textContent && sizeSelector.value === sizeList[0].textContent && colorSelector.value === colorList[3].textContent) {
-        
-        document.querySelector('.stock-info').innerHTML = template(cleatsList[8]);
-        
-    } else if (brandSelector.value === brandList[4].textContent && sizeSelector.value === sizeList[1].textContent && colorSelector.value === colorList[0].textContent) {
-        
-        document.querySelector('.stock-info').innerHTML = template(cleatsList[9]);
-        
+    document.querySelector('.admin-login').style.display = 'inline-block';
+});
+document.querySelector('.close-admin-btn').addEventListener('click', function () {
+    'use strict';
+    userName.style.backgroundColor = 'white';
+    passWord.style.backgroundColor = 'white';
+    document.querySelector('.error-msg').style.display = 'none';
+    document.querySelector('.admin-login').style.display = 'none';
+});
+document.querySelector('.login-btn').addEventListener('click', function () {
+    'use strict';
+    var adminFound = false;
+    for (var i = 0; i < admin.length; i++) {
+        if (userName.value === admin[i].usrnm && passWord.value === admin[i].pswd) {
+            document.querySelector('.stock-adder').style.display = 'inline-block';
+            userName.style.backgroundColor = 'white';
+            passWord.style.backgroundColor = 'white';
+            document.querySelector('.error-msg').style.display = 'none';
+            adminFound = true;
+        }
+        if (adminFound === false) {
+            document.querySelector('.error-msg').innerHTML = '<p>Wrong password or username!</p>';
+            userName.style.backgroundColor = 'rgba(255, 0, 0,0.5)';
+            passWord.style.backgroundColor = 'rgba(255, 0, 0,0.5)';
+            document.querySelector('.error-msg').style.display = 'block';
+        }
+    }
+    userName.value = "";
+    passWord.value = "";
+});
+document.querySelector('.add-admin-btn').addEventListener('click', function () {
+    'use strict';
+    var adminFound = false;
+    for (var i = 0; i < admin.length; i++) {
+        if (userName.value === admin[0].usrnm && passWord.value === admin[0].pswd && userName.value !== "" && passWord.value !== "") {
+            document.querySelector('.add-admin').style.display = 'inline-block';
+            userName.style.backgroundColor = 'white';
+            passWord.style.backgroundColor = 'white';
+            document.querySelector('.error-msg').style.display = 'none';
+            adminFound = true;
+        }
+        if (adminFound === false) {
+            document.querySelector('.error-msg').innerHTML = '<p>Please contact your Admin to add new Staff!</p>';
+            userName.style.backgroundColor = 'rgba(255, 0, 0,0.5)';
+            passWord.style.backgroundColor = 'rgba(255, 0, 0,0.5)';
+            document.querySelector('.error-msg').style.display = 'block';
+        }
+    }
+    userName.value = "";
+    passWord.value = "";
+});
+document.querySelector('.close-staff-adder').addEventListener('click', function () {
+    'use strict';
+    document.querySelector('.add-admin').style.display = 'none';
+});
+document.querySelector('.add-staff-btn').addEventListener('click', function () {
+    'use strict';
+    var newUser = document.querySelector('.new-username');
+    var newPswd = document.querySelector('.new-password');
+    var newUsername= {usrnm: newUser.value, pswd: newPswd.value};
+    if (newUser.value !== "" && newPswd.value !== "" && newPswd.value.length >= 6) {
+        admin.push(newUsername);
     } else {
-        document.querySelector('.stock-info').innerHTML = '<h3>Sorry, we currently do not have those in storage!</h3>'
+        alert('Please Enter a valid Username, Password must be atleast Six letter/numbers long');
     }
-    
-//    localStorage.setItem('stock-info', document.querySelector('.stock-info').innerHTML);
+    newUser.value = "";
+    newPswd.value = "";
 });
-
-document.querySelector('.show-all-stock-btn').addEventListener('click', function () {
-    var myTemplate = document.querySelector('.myTemplate').innerHTML;
-    
-    var template = Handlebars.compile(myTemplate);
-    
-    var cleats = {list: cleatsList}
-        
-    document.querySelector('.all-cleats-in-stock').innerHTML = template(cleats);
-    document.querySelector('.all-cleats-in-stock').style.display = 'block';
-});
-
-document.querySelector('.hide-all-stock-btn').addEventListener('click', function () {
-   document.querySelector('.all-cleats-in-stock').style.display = 'none'; 
-});
-
 document.querySelector('.close-btn').addEventListener('click', function () {
-//    alert('tester');
+    'use strict';
     document.querySelector('.stock-adder').style.display = 'none';
 });
-
-document.querySelector('.add-stock-btn').addEventListener('click', function () {
-    document.querySelector('.stock-adder').style.display = 'block';
-});
-
 document.querySelector('.add-btn').addEventListener('click', function () {
-    if (addBrand.value !== "" && addSize.value !== "" && addStock.value !== "" && addColor.value !== "" && addPrice.value !== "") {
-        var newStock = {brand: addBrand.value, size: addSize.value, stock: addStock.value, price: addPrice.value, color: addColor.value};
-        addedStock = newStock;
-//    alert(addedStock);
-        cleatsList.push(addedStock);
-//    alert(JSON.stringify(cleatsList));
-        newBrand = document.createElement('option');
-        newSize = document.createElement('option');
-        newColor = document.createElement('option');
-        newStockN = document.createElement('option');
-        newPrice = document.createElement('option');
-    
-        brandText = document.createTextNode(addBrand.value);
-        sizeText = document.createTextNode(addSize.value);
-        colorText = document.createTextNode(addColor.value);
-        stockText = document.createTextNode(addStock.value);
-        priceText = document.createTextNode(addPrice.value);
-    
-        newBrand.appendChild(brandText);
-        newSize.appendChild(sizeText);
-        newColor.appendChild(colorText);
-        newStockN.appendChild(stockText);
-        newPrice.appendChild(priceText);
-        
-        brandSelector.appendChild(newBrand);
-        sizeSelector.appendChild(newBrand);
-        colorSelector.appendChild(newBrand);
+    'use strict';
+    var brandAdd = document.querySelector('.brandAdd');
+    var sizeAdd = document.querySelector('.sizeAdd');
+    var colorAdd = document.querySelector('.colorAdd'); 
+    var stockAdd = document.querySelector('.stockAdd');
+    var priceAdd = document.querySelector('.priceAdd');
+    if (brandAdd.value !== "" && sizeAdd.value !== "" && colorAdd.value !== "" && stockAdd.value !== "") {
+        var newStock = {brand: brandAdd.value, size: sizeAdd.value, color: colorAdd.value, stock: stockAdd.value, price: priceAdd.value};
+        stock.push(newStock);
     } else {
-        alert('Error')
+        alert('Please enter valid Stock Items');
     }
+    brandAdd.value = "";
+    sizeAdd.value = "";
+    colorAdd.value = "";
+    stockAdd.value = "";
+    priceAdd.value = "";
+    
+    var showTemplate = document.querySelector('.show-stock-template').innerHTML;
+    var temp = Handlebars.compile(showTemplate);
+    document.querySelector('.display-all-stock').innerHTML = temp(data);
+    
+    if (document.querySelector('.display-all-stock').style.display === 'inline-block') {
+        document.querySelector('.display-all-stock').style.display = 'none';
+    }
+    
+    document.querySelector('.stock-checker').innerHTML = template(data);
 });
